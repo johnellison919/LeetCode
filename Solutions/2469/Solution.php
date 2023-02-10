8e4f7f6121dac22cd1f5f2f2ca125c18856243bc
John Ellison <johnellison919@gmail.com>
2023-02-10 16:45:48 -0600

    Adds the Solution to Problem 2469
    Adds the Solution to Problem 2469 - written in PHP

---
+++ Solutions/2469/Solution.php
@@ -0,0 +1,20 @@
+class Solution {
+
+    /**
+     * @param Float $celsius
+     * @return Float[]
+     */
+    function convertTemperature($celsius): Array{
+        $answer = [];
+
+        // Convert to K
+        $kelvin = $celsius + 273.15;
+        $answer[] = $kelvin;
+
+        // Convert to F
+        $fahrenheit = $celsius * 1.80 + 32.00;
+        $answer[] = $fahrenheit;
+
+        return $answer;
+    }
+}
